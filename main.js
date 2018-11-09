54cad47486b9f4de0172d61adad57a24a37a22f7
Mac Ordan <marcordan@gmail.com>
2018-11-09 16:47:02 +0800

    Fetch bookmarks from localstorage

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
 // listen to form submit
-document.getElementById('myForm').addEventListener('submit', saveBookMark);
+document.getElementById("myForm").addEventListener("submit", saveBookMark);
 
-function saveBookMark(e){
-   // Get form values
-   var siteName = document.getElementById('siteName').value;
-   var siteUrl = document.getElementById('siteUrl').value;
+function saveBookMark(e) {
+  // Get form values
+  var siteName = document.getElementById("siteName").value;
+  var siteUrl = document.getElementById("siteUrl").value;
 
-    var bookmark = {
-        name: siteName,
-        url: siteUrl
-    }
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
 
-    // Local storage test
-    // localStorage.setItem('test','Hello World');
-    // console.log(localStorage.getItem('test'));
-    // localStorage.removeItem('test');
-    // console.log(localStorage.getItem('test'));
+  // Local storage test
+  // localStorage.setItem('test','Hello World');
+  // console.log(localStorage.getItem('test'));
+  // localStorage.removeItem('test');
+  // console.log(localStorage.getItem('test'));
 
+  // Test if bookmarks is null
+  if (localStorage.getItem("bookmarks") === null) {
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localstorage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Re-set back to localstorage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
 
-    // Test if bookmarks is null
-    if (localStorage.getItem('bookmarks') === null){
-        // Init array
-        var bookmarks = [];
-        // Add to array
-        bookmarks.push(bookmark);
-        // Set to localstorage
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-    } else {
-        // Get bookmarks from localStorage
-        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-        // Add bookmark to array
-        bookmarks.push(bookmark);
-        // Re-set back to localstorage
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-    }
+  // Prevent form from submitting
+  e.preventDefault();
+}
 
+function fetchBookmarks() {
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
 
-    //Prevent form from submitting
-    e.preventDefault();
+  // Get output id
+  var bookmarksResults = document.getElementById("bookmarksResults");
+
+  // Build Output
+  bookmarksResults.innerHTML = "";
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+  bookmarksResults.innerHTML += '<div class = "well">'+
+                                '<h3>'+name+
+                                '</h3>'+
+                                '</div>';
+  }
 }
